4938953c8061e8bdf29047d157a4b8979eb869e7
bloodtypebpos <matt.tigrett@gmail.com>
2019-09-16 01:35:17 -0400

    Update kioskTabber.py

--- kioskTabber.py
+++ kioskTabber.py
@@ -1,27 +1,35 @@
 import pyautogui, time, threading
 
 theTab = 0
-nSeconds = 0.1
+nSeconds = 0.2
 sleeperTime = 5
-timeCounter = 50
-timeReCount = 50
+timeCounter = 100
+timeReCount = 1200
 waitBool = True
 screenWidth, screenHeight = pyautogui.size()
 currentMouseX, currentMouseY = pyautogui.position()
 lastMouseX, lastMouseY = (0,0)
+numTabs = 4
 
 def timerCheck():
     global timeCounter
     global theTab
     global waitBool
+    global numTabs
     threading.Timer(nSeconds,timerCheck).start()
     if waitBool:
         helloWorld()
     timeCounter = timeCounter - 1
+    if theTab == 2:
+        pyautogui.scroll(-5)
     if timeCounter < 0:
         pyautogui.hotkey('ctrl','tab')
-        theTab = (theTab + 1)%3
-        timeCounter = timeReCount
+        theTab = (theTab + 1)%numTabs
+        if theTab == 2:
+            pyautogui.press('home')
+            timeCounter = 1800
+        else:
+            timeCounter = timeReCount
     
 def helloWorld():
     global lastMouseX
@@ -46,11 +54,13 @@ def helloWorld():
 def tabQuit():
     global theTab
     global waitBool
+    global numTabs
     waitBool = False
     while theTab != 0:
-        pyautogui.press('home')
+        if theTab != 1:
+            pyautogui.press('home')
         pyautogui.hotkey('ctrl','tab')
-        theTab = (theTab + 1)%3
+        theTab = (theTab + 1)%numTabs
     waitBool = True
     
 time.sleep(5)    
