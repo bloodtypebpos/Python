39c8fc04acadc8ceaf5e1baf01242d7a572aded7
bloodtypebpos <matt.tigrett@gmail.com>
2020-09-30 20:28:38 -0400

    Create spherefit.py

---
+++ spherefit.py
@@ -0,0 +1,339 @@
+words = r"""1,1,171.5357,-251.0962,-422.3531,-0.97790,0.19299,-0.08036
+1,1,172.0971,-251.5233,-427.4451,-0.96890,0.18607,-0.16314
+1,1,173.2563,-251.6141,-432.8525,-0.95020,0.18462,-0.25106
+1,1,174.6879,-253.4790,-438.4330,-0.92701,0.15432,-0.34181
+1,1,177.9618,-252.9632,-445.6162,-0.87366,0.16268,-0.45853
+1,1,180.5548,-229.4523,-424.1415,-0.83140,0.54478,-0.10944
+1,1,180.9829,-230.5687,-430.1025,-0.82460,0.52674,-0.20636
+1,1,181.7166,-233.1401,-437.2616,-0.81277,0.48499,-0.32278
+1,1,184.5010,-235.1421,-445.3643,-0.76737,0.45237,-0.45443
+1,1,193.3120,-215.2087,-422.9156,-0.62404,0.77625,-0.08951
+1,1,193.1860,-216.8343,-430.4818,-0.62628,0.75007,-0.21254
+1,1,193.9591,-219.0663,-438.1644,-0.61371,0.71378,-0.33744
+1,1,195.6664,-221.9370,-445.7237,-0.58587,0.66701,-0.46027
+1,1,213.8148,-204.4991,-424.2012,-0.29084,0.95038,-0.11041
+1,1,215.1966,-205.7518,-432.7856,-0.26846,0.93028,-0.25001
+1,1,215.6093,-208.3323,-440.6289,-0.26173,0.88826,-0.37749
+1,1,215.4903,-211.9535,-447.7159,-0.26364,0.82932,-0.49267
+1,1,234.9968,-201.7628,-422.7786,0.05344,0.99475,-0.08728
+1,1,234.6659,-203.1116,-431.2505,0.04808,0.97316,-0.22504
+1,1,234.5660,-206.0237,-440.4836,0.04645,0.92580,-0.37514
+1,1,233.4164,-209.1552,-447.1691,0.02776,0.87476,-0.48375
+1,1,262.3784,-209.9192,-422.9187,0.49848,0.86226,-0.08956
+1,1,260.0859,-210.1182,-431.0209,0.46131,0.85920,-0.22129
+1,1,260.0070,-212.6093,-438.5233,0.46009,0.81882,-0.34330
+1,1,257.8063,-215.2080,-446.0869,0.42420,0.77637,-0.46616
+1,1,281.6103,-227.3770,-422.8129,0.81097,0.57846,-0.08783
+1,1,279.7886,-228.0553,-433.3139,0.78163,0.56762,-0.25858
+1,1,276.7643,-228.5505,-441.2642,0.73245,0.55957,-0.38782
+1,1,275.5744,-231.4988,-446.9087,0.71300,0.51156,-0.47951
+1,1,290.8108,-246.8299,-423.1448,0.96047,0.26231,-0.09323
+1,1,289.1535,-246.3384,-431.8201,0.93381,0.27038,-0.23428
+1,1,286.9070,-247.5624,-439.7703,0.89728,0.25048,-0.36351
+1,1,283.6648,-249.2948,-447.3943,0.84443,0.22228,-0.48736
+1,1,292.4192,-270.8709,-423.5205,0.98673,-0.12839,-0.09935
+1,1,290.6903,-271.2116,-432.8171,0.95881,-0.13396,-0.25049
+1,1,288.3227,-272.0090,-439.7035,0.92035,-0.14692,-0.36245
+1,1,283.8424,-274.3077,-448.0617,0.84727,-0.18424,-0.49818
+1,1,284.4830,-293.9883,-423.5736,0.85778,-0.50415,-0.10021
+1,1,282.7422,-293.8369,-432.4641,0.82964,-0.50178,-0.24476
+1,1,279.8250,-293.4455,-440.6494,0.78220,-0.49540,-0.37782
+1,1,275.2233,-293.3251,-448.5763,0.70717,-0.49329,-0.50653
+1,1,266.3893,-312.9805,-426.3813,0.56377,-0.81295,-0.14587
+1,1,263.1553,-312.2663,-436.4913,0.51127,-0.80146,-0.31026
+1,1,261.8815,-309.5252,-443.9913,0.49048,-0.75677,-0.43213
+1,1,261.3694,-306.5167,-449.1938,0.48203,-0.70768,-0.51657
+1,1,248.9217,-321.3866,-426.1150,0.27980,-0.94957,-0.14153
+1,1,248.4395,-319.8916,-433.6272,0.27202,-0.92545,-0.26370
+1,1,249.1268,-316.5307,-442.1192,0.28321,-0.87084,-0.40178
+1,1,246.8621,-313.1541,-449.6341,0.24626,-0.81553,-0.52371
+1,1,230.3146,-323.7245,-426.9628,-0.02266,-0.98760,-0.15532
+1,1,229.4475,-321.9564,-434.6917,-0.03676,-0.95900,-0.28100
+1,1,231.2919,-319.6155,-441.3940,-0.00678,-0.92082,-0.38992
+1,1,231.3021,-315.9796,-448.6515,-0.00661,-0.86147,-0.50776
+1,1,198.5915,-314.2246,-425.2122,-0.53833,-0.83313,-0.12686
+1,1,198.0168,-312.4758,-431.4826,-0.54771,-0.80477,-0.22880
+1,1,200.4293,-310.5950,-440.5984,-0.50848,-0.77417,-0.37698
+1,1,176.5047,-289.4538,-423.4843,-0.89721,-0.43041,-0.09875
+1,1,177.8056,-287.6945,-433.7500,-0.87629,-0.40192,-0.26567
+1,1,181.6090,-284.8278,-445.6462,-0.81433,-0.35526,-0.45898
+1,1,184.8059,-284.8462,-450.7030,-0.76218,-0.35547,-0.54104
+1,1,171.4300,-273.9058,-423.1156,-0.97970,-0.17772,-0.09276
+1,1,173.2228,-273.9633,-432.9802,-0.95078,-0.17869,-0.25315
+1,1,176.2555,-272.2768,-442.3623,-0.90143,-0.15127,-0.40564
+1,1,180.0546,-271.1365,-449.8366,-0.83944,-0.13269,-0.52700"""
+
+# tip diameter (as close as possible) as read on the CMM
+tipDia = 0.1575    #0.15739 @ 0.00008    #0.15736 @ 0.00003   #0.15713 @ 0.00001
+
+# Diameter of EM Ball that EM Part is intended for [EM150 = 4, EM300 = 5]            
+sphereSize = 5
+
+# Did you measure the top cap or the ball? Top Cap = "Cup", Ball = "Ball"
+cupOrBall = "Cup"  # "Ball" or "Cup"    **Note: if you put anything other than "Cup" it defaults to the ball
+
+# This is a scalar for the whiskers. Should be between 0-0.75ish
+wlen = 0.05
+
+# Number of decimals to display
+numDigits = 5
+
+
+numSkips = 0
+##################################################################################################
+import numpy as np
+#    fit a sphere to X,Y, and Z data points
+#    returns the radius and center points of
+#    the best fit sphere
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+##################################################################################################
+def num2num(OldValue,OldMin,OldMax,NewMin,NewMax):
+    newValue = (((float(OldValue) - float(OldMin)) * (float(NewMax) - float(NewMin))) / (float(OldMax) - float(OldMin))) + float(NewMin)
+    return newValue
+
+def sphereFit(spX,spY,spZ,tipDia,sphereSize,cupOrBall):
+    #   Assemble the A matrix
+    spX = np.array(spX)
+    spY = np.array(spY)
+    spZ = np.array(spZ)
+    A = np.zeros((len(spX),4))
+    A[:,0] = spX*2
+    A[:,1] = spY*2
+    A[:,2] = spZ*2
+    A[:,3] = 1
+    #   Assemble the f matrix
+    f = np.zeros((len(spX),1))
+    f[:,0] = (spX*spX) + (spY*spY) + (spZ*spZ)
+    C, residules, rank, singval = np.linalg.lstsq(A,f,rcond=None)
+    #   solve for the radius
+    t = (C[0]*C[0])+(C[1]*C[1])+(C[2]*C[2])+C[3]
+    radius = math.sqrt(t)
+    #print(radius)
+    tipRad = tipDia/2
+    if cupOrBall == "Cup":
+        scaleVal = (2*radius+tipDia)/(2*radius)
+    else:
+        scaleVal = (2*radius-tipDia)/(2*radius)
+    x0 = C[0][0]
+    y0 = C[1][0]
+    z0 = C[2][0]
+    sx = scaleVal
+    sy = scaleVal
+    sz = scaleVal
+    S = [[sx,0,0,0],[0,sy,0,0],[0,0,sz,0],[0,0,0,1]]
+    T1 = [[1,0,0,-x0],[0,1,0,-y0],[0,0,1,-z0],[0,0,0,-z0]]
+    T2 = [[1,0,0,x0],[0,1,0,y0],[0,0,1,z0],[0,0,0,z0]]
+    
+    X = []
+    Y = []
+    Z = []
+    for i in range(0,len(spX)):
+        pnt = [spX[i],spY[i],spZ[i],1]
+        pxv = np.matmul(T1,pnt)
+        vxs = np.matmul(S,pxv)
+        sxp = np.matmul(T2,vxs)
+        X.append(sxp[0])
+        Y.append(sxp[1])
+        Z.append(sxp[2])
+    spX = np.array(X)
+    spY = np.array(Y)
+    spZ = np.array(Z)
+    A = np.zeros((len(spX),4))
+    A[:,0] = spX*2
+    A[:,1] = spY*2
+    A[:,2] = spZ*2
+    A[:,3] = 1
+    #   Assemble the f matrix
+    f = np.zeros((len(spX),1))
+    f[:,0] = (spX*spX) + (spY*spY) + (spZ*spZ)
+    C, residules, rank, singval = np.linalg.lstsq(A,f,rcond=None)
+    #   solve for the radius
+    t = (C[0]*C[0])+(C[1]*C[1])+(C[2]*C[2])+C[3]
+    radius = math.sqrt(t)
+    return radius, C[0], C[1], C[2], spX, spY,spZ
+
+def whiskers(spX,spY,spZ,wNums,scaleVal):
+    X = []
+    Y = []
+    Z = []
+    for i in range(0,len(spX)):
+        scl = abs(wNums[i])*scaleVal
+        #print(scl)
+        if wNums[i] < 0:
+            S = [[-scl,0,0,0],[0,-scl,0,0],[0,0,-scl,0],[0,0,0,1]]
+        else:
+            S = [[scl,0,0,0],[0,scl,0,0],[0,0,scl,0],[0,0,0,1]]
+        pnt = [spX[i],spY[i],spZ[i],1]
+        sxp = np.matmul(S,pnt)
+        X.append(sxp[0])
+        Y.append(sxp[1])
+        Z.append(sxp[2])
+    spX = np.array(X)
+    spY = np.array(Y)
+    spZ = np.array(Z)
+    
+    return spX,spY,spZ
+
+##################################################################################################
+
+mm2in = 1/25.4
+
+wards = words.splitlines()
+coords = []
+for word in wards:
+    line = word.split(',')
+    coord = [float(line[2]),float(line[3]),float(line[4])]
+    coords.append(coord)
+
+coords = sorted(coords,key=lambda point:point[0])
+spX = []
+spY = []
+spZ = []
+for coord in coords:
+    spX.append(coord[0]*mm2in)
+    spY.append(coord[1]*mm2in)
+    spZ.append(coord[2]*mm2in)
+
+R, x0, y0, z0, X, Y, Z = sphereFit(spX,spY,spZ,tipDia,sphereSize,cupOrBall)
+cNums = []
+wNums = []
+origin = np.array([0,0,0])
+for i in range(0,len(X)):
+    X[i] = X[i] - x0
+    Y[i] = Y[i] - y0
+    Z[i] = Z[i] - z0
+    pnt = np.array([X[i],Y[i],Z[i]])
+    cNums.append(np.linalg.norm(pnt-origin)-(sphereSize/2))
+    wNums.append(np.linalg.norm(pnt-origin)-(sphereSize/2))
+
+
+
+
+
+
+
+
+
+if numSkips > 0:
+    top_idx = np.argsort(cNums)[-numSkips:]
+    bottom_idx = np.argsort(cNums)[:numSkips]
+    baddies = []
+    for i in range(0,len(bottom_idx)):
+        baddies.append(bottom_idx[i])
+    for i in range(0,len(top_idx)):
+        baddies.append(top_idx[i])
+    print(baddies)
+    wards = words.splitlines()
+    coords = []
+    skipNum = 0
+    for word in wards:
+        if skipNum in baddies:
+            pass
+        else:
+            line = word.split(',')
+            coord = [float(line[2]),float(line[3]),float(line[4])]
+            coords.append(coord)
+        skipNum = skipNum + 1
+    
+    coords = sorted(coords,key=lambda point:point[0])
+    spX = []
+    spY = []
+    spZ = []
+    for coord in coords:
+        spX.append(coord[0]*mm2in)
+        spY.append(coord[1]*mm2in)
+        spZ.append(coord[2]*mm2in)
+    
+    R, x0, y0, z0, X, Y, Z = sphereFit(spX,spY,spZ,tipDia,sphereSize,cupOrBall)
+    cNums = []
+    wNums = []
+    origin = np.array([0,0,0])
+    for i in range(0,len(X)):
+        X[i] = X[i] - x0
+        Y[i] = Y[i] - y0
+        Z[i] = Z[i] - z0
+        pnt = np.array([X[i],Y[i],Z[i]])
+        cNums.append(np.linalg.norm(pnt-origin)-(sphereSize/2))
+        wNums.append(np.linalg.norm(pnt-origin)-(sphereSize/2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cMin = min(cNums)
+cMax = max(cNums)
+for i in range(0,len(cNums)):
+    cNums[i] = num2num(cNums[i], cMin, cMax, 0.001, 0.999)
+
+# Create a sphere
+r = sphereSize/2
+pi = np.pi
+cos = np.cos
+sin = np.sin
+phi, theta = np.mgrid[0.0:pi:25j, 0.0:2.0*pi:25j]
+x = r*sin(phi)*cos(theta)
+y = r*sin(phi)*sin(theta)
+z = r*cos(phi)
+#############################################
+# Make whiskers
+scaleVal = (1/(max(wNums)-min(wNums)))/2
+scaleVal = scaleVal*wlen
+#print("----------------------")
+#print(scaleVal)
+#print("----------------------")
+wX, wY, wZ = whiskers(X, Y, Z, wNums, scaleVal)
+
+norm = matplotlib.colors.Normalize()
+norm.autoscale(cNums)
+cm = matplotlib.cm.rainbow
+sm = matplotlib.cm.ScalarMappable(cmap=cm, norm=norm)
+sm.set_array([])
+
+
+header = "Actual Radius: " + str(round(R,numDigits)) + '         Intended Radius: ' + str(sphereSize/2) + '       Points: ' + str(len(X)) 
+header = header +'\nDifference: ' + str(round(R-(sphereSize/2),numDigits)) + '        Form Error: ' + str(round(max(wNums)-min(wNums),numDigits)) + '                    '
+header = header + '\nMax Point: ' + str(round(max(wNums),numDigits)) + '          Min Point: ' + str(round(min(wNums),numDigits)) + '                    '
+
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x, y, z,  rstride=1, cstride=1, color='Gray', alpha=0.05, linewidth=0)
+ax.scatter(X,Y,Z,c=cNums,cmap=cm,s=50)
+ax.quiver(X,Y,Z,wX,wY,wZ,color=cm(norm(cNums)),arrow_length_ratio=0)
+# Make perspective or aspect ratio or whatever you wanna call it to be equal
+ax.set_aspect('equal')
+# Set axis limits 
+ax.set_xlim3d(-sphereSize/2, sphereSize/2)
+ax.set_ylim3d(-sphereSize/2, sphereSize/2)
+ax.set_zlim3d(-sphereSize/2, sphereSize/2)
+# Turn off background
+ax.xaxis.set_pane_color((1,1,1,0))
+ax.yaxis.set_pane_color((1,1,1,0))
+ax.zaxis.set_pane_color((1,1,1,0))
+# Turn off grid lines
+ax.xaxis._axinfo['grid']['color'] = (1,1,1,0)
+ax.yaxis._axinfo['grid']['color'] = (1,1,1,0)
+ax.zaxis._axinfo['grid']['color'] = (1,1,1,0)
+# Turn off axes
+ax.axis('off')
+
+plt.title(header)
+norm.autoscale(wNums)
+cb = matplotlib.cm.ScalarMappable(cmap=cm, norm=norm)
+cb.set_array([])
+plt.colorbar(cb)
+plt.show()
