b136b18e59e39b54142989fc28233e35eccd127e
bloodtypebpos <matt.tigrett@gmail.com>
2021-06-16 16:42:17 -0400

    Create UTM.py

---
+++ Present Value/UTM.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 19:09:32 2021
+
+@author: james
+"""
+
+import math
+
+def CalculateDistance(n1, e1, n2, e2):
+    return math.sqrt((n1-n2)**2 + (e1-e2)**2)
+
+def CalculateBearing(n1, e1, n2, e2):
+    b = 0
+    if (n2 > n1):
+        if (e2 > e1):
+            b = 57.2958* math.atan((n2-n1)/(e2-e1))
+        elif (e1 > e2):
+            b = 57.2958* math.atan((n2-n1)/(e2-e1))+180
+    elif (n1 > n2):
+        if (e2 > e1):
+            b = 57.2958* math.atan((n2-n1)/(e2-e1))+360
+        elif (e1 > e2):
+            b = 57.2958* math.atan((n2-n1)/(e2-e1))
+    return b
+
+
+
+
+#Location A - Front of the ATC
+#29.2320, -81.0982
+#17R 0490457E 3233696N
+
+#Location B - Racetrac on LPGA Blvd
+#9.2236, -81.0929
+#17R 0490967E 3232763N
+
+#Location C - Buc-ees
+#29.2239, -81.1004
+#17R 0490240E 3232801N
+
+LocA = ["17R", "0490457E", "3233696N"]
+LocB = ["17R", "0490967E", "3232763N"]
+LocC = ["17R", "0490240E", "3232801N"]
+
+
+
+
+
+def dist2(L1,L2):
+    if L1[0] != L2[0]:
+        print("These coordinates are not in the same UTM Zone")
+    else:
+        x1 = int(L1[1][:-1])
+        y1 = int(L1[2][:-1])
+        x2 = int(L2[1][:-1])
+        y2 = int(L2[2][:-1])
+        dx = x1-x2
+        dy = y1-y2
+        d = math.sqrt(dx**2 + dy**2)
+        print("Locations:")
+        print(L1)
+        print(L2)
+        print("Distance: " + str(round(d,2)) + " m")
+        eastBool = True
+        direction = "Direction: "
+        if dy < 0:
+            direction = direction + "North-"
+        else:
+            direction = direction + "South-"
+        if dx < 0:
+            direction = direction + "East"
+        else:
+            direction = direction + "West"
+            eastBool = False
+        print(direction)
+        b = round(math.degrees(math.atan(dx/dy)),1)
+        bearings = "Bearings: "
+        if eastBool:
+            bearings = bearings + str(b+180) + "°"
+        else:
+            bearings = bearings + str(b-180) + "°"
+        print(bearings)
+        print(b+360)
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+d1 = CalculateDistance(3696,457,2763,967)
+d2 = CalculateDistance(763,967,801,240)
+d3 = CalculateDistance(2801,240,3696,457)
+
+
+dist2(LocB,LocC)
+#dist2(LocB,LocC)
+#dist2(LocC,LocA)
+
+print("-----------------------------------------------------------")
+
+b1 = CalculateBearing(3696,457,2763,967)
+b2 = CalculateBearing(763,967,801,240)
+b3 = CalculateBearing(2801,240,3696,457)
+
+print(d1, b1, d2, b2, d3, b3)
