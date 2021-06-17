4ac07fe470674fdf1ce8a06c05220f94248c542b
bloodtypebpos <matt.tigrett@gmail.com>
2021-06-17 03:09:00 -0400

    Update UTM.py

--- Present Value/UTM.py
+++ Present Value/UTM.py
@@ -1,25 +1,5 @@
 import math
 
-def CalculateDistance(n1, e1, n2, e2):
-    return math.sqrt((n1-n2)**2 + (e1-e2)**2)
-
-def CalculateBearing(n1, e1, n2, e2):
-    b = 0
-    if (n2 > n1):
-        if (e2 > e1):
-            b = 57.2958* math.atan((n2-n1)/(e2-e1))
-        elif (e1 > e2):
-            b = 57.2958* math.atan((n2-n1)/(e2-e1))+180
-    elif (n1 > n2):
-        if (e2 > e1):
-            b = 57.2958* math.atan((n2-n1)/(e2-e1))+360
-        elif (e1 > e2):
-            b = 57.2958* math.atan((n2-n1)/(e2-e1))
-    return b
-
-
-
-
 #Location A - Front of the ATC
 #29.2320, -81.0982
 #17R 0490457E 3233696N
@@ -36,13 +16,13 @@ LocA = ["17R", "0490457E", "3233696N"]
 LocB = ["17R", "0490967E", "3232763N"]
 LocC = ["17R", "0490240E", "3232801N"]
 
-
-
+LocD = ["17R", "0490457E", "3232763N"]  # Loc A's Easting and Loc B's Northing
+LocE = ["17R", "0490967E", "3233696N"]  # Loc B's Easting and Loc B's Northing 
 
 
 def dist2(L1,L2):
-    if L1[0] != L2[0]:
-        print("These coordinates are not in the same UTM Zone")
+    if L1 == L2:
+        print("These two coordinates are the same!")
     else:
         x1 = int(L1[1][:-1])
         y1 = int(L1[2][:-1])
@@ -58,30 +38,87 @@ def dist2(L1,L2):
         eastBool = True
         northBool = True
         direction = "Direction: "
-        if dy < 0:
-            direction = direction + "North-"
+        if dy == 0:
+            pass
+        elif dy < 0:
+            direction = direction + "North "
         else:
-            direction = direction + "South-"
+            direction = direction + "South "
             northBool = False
-        if dx < 0:
+        if dx == 0:
+            pass
+        elif dx < 0:
             direction = direction + "East"
         else:
             direction = direction + "West"
             eastBool = False
         print(direction)
-        b = round(math.degrees(math.atan(dx/dy)),1)
+        
+        # If dy = 0, then atan(dx/dy) gives error
+        # However, we know if dy = 0 then we're just moving EAST or WEST
+        # So we just set the direction to 90* or 270* if dy = 0
+        if dy == 0:
+            if dx < 0:
+                b = round(math.degrees(math.pi/2),1)
+            else:
+                b = round(math.degrees((3*math.pi)/2),1)
+        else:
+            b = round(math.degrees(math.atan(dx/dy)),1)
+        
+        
         bearings = "Bearings: "
         if eastBool == True:
             if northBool == True:
-                bearings = bearings + str(b+180)
+                #North-East
+                b = b
+                #bearings = bearings + str(b)
             else:
-                bearings = bearings + str(b-180)
+                #South-East
+                b = b + 180
+                #bearings = bearings + str(b+180)
         else:
             if northBool == True:
-                bearings = bearings + str(b-180)
+                #North-West
+                b = b + 360
+                #bearings = bearings + str(b+360)
             else:
-                bearings = bearings + str(b+360)
+                #South-West
+                b = b + 180
+                #bearings = bearings + str(b+180)
+        if b > 360:
+            b = b-360
+        bearings = bearings + str(b)
         print(bearings)
 
+print("-----------------------------------------------------------")
+# North-West
+dist2(LocB,LocA)
+print("-----------------------------------------------------------")
+# North-East
+dist2(LocC,LocA)
+print("-----------------------------------------------------------")
+# South-West
+dist2(LocA,LocC)
+print("-----------------------------------------------------------")
+# South-East
 dist2(LocA,LocB)
 print("-----------------------------------------------------------")
+# South
+dist2(LocA,LocD)
+print("-----------------------------------------------------------")
+# North
+dist2(LocD,LocA)
+print("-----------------------------------------------------------")
+# East
+dist2(LocA,LocE)
+print("-----------------------------------------------------------")
+# West
+dist2(LocE,LocA)
+print("-----------------------------------------------------------")
+# Same Location
+dist2(LocA,LocA)
+print("-----------------------------------------------------------")
+
+
+
+
