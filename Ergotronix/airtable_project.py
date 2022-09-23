73637d10681914bdcf2a47bb44a5f2e87952d79c
bloodtypebpos <matt.tigrett@gmail.com>
2022-09-23 08:07:36 -0400

    Update airtable_project.py

--- Ergotronix/airtable_project.py
+++ Ergotronix/airtable_project.py
@@ -1,3 +1,6 @@
+from airtable import Airtable
+
+
 class Project:
     def __init__(self, base_key, api_key, conn, table, sonum):
         self.base_key = base_key
@@ -158,3 +161,110 @@ class Project4:
             self.items.append(item)
 
 
+class Airtable_DB:
+    def __init__(self, base_key, api_key, conn, c, tab_name, view_name, table_name, ints, floats, ignores):
+        self.base_key = base_key
+        self.api_key = api_key
+        self.conn = conn
+        self.c = c
+        self.tab_name = tab_name
+        self.view_name = view_name
+        self.table_name = table_name
+        self.ints = ints
+        self.floats = floats
+        self.ignores = ignores
+
+        airtable = Airtable(self.base_key, self.tab_name, self.api_key)
+        records = airtable.get_all(view=self.view_name)
+        fields = set()
+        for record in records:
+            for field in list(record['fields'].keys()):
+                fields.add(field)
+        fields = list(fields)
+        ints = self.ints
+        floats = self.floats
+        ignores = self.ignores
+
+        recs = []
+        for record in records:
+            rec = []
+            for field in fields:
+                try:
+                    if field in ints:
+                        rec.append(int(record['fields'][field]))
+                    elif field in floats:
+                        rec.append(float(record['fields'][field]))
+                    elif field in ignores:
+                        if field == 'Attachments':
+                            rec.append(record['fields'][field][0]['url'])
+                        else:
+                            pass
+                    else:
+                        rec.append(record['fields'][field])
+                except:
+                    if field in ints:
+                        rec.append(0)
+                    elif field in floats:
+                        rec.append(0.0)
+                    elif field in ignores:
+                        if field == 'Attachments':
+                            rec.append('NA')
+                        else:
+                            pass
+                    else:
+                        rec.append("NA")
+            recs.append(rec)
+
+        table_name = self.table_name
+        self.make_air_table(fields, table_name, ints, floats, ignores)
+        self.fill_air_table(fields, table_name, recs, ints, floats, ignores)
+
+    def make_air_table(self, fields, table_name, ints, floats, ignores):
+        for i in range(0, len(fields)):
+            if '"' in fields[i]:
+                fields[i].replace('"', '``')
+            if "'" in fields[i]:
+                fields[i].replace("'", '`')
+        self.c.execute('DROP TABLE IF EXISTS "' + table_name + '"')
+        query = 'CREATE TABLE "' + table_name + '"(id INTEGER PRIMARY KEY,'
+        for field in fields:
+            if field in ints:
+                query = query + ' "' + field + '" INTEGER,'
+            elif field in floats:
+                query = query + ' "' + field + '" REAL,'
+            elif field in ignores:
+                if field == 'Attachments':
+                    query = query + ' "' + field + '" TEXT,'
+                else:
+                    pass
+            else:
+                query = query + ' "' + field + '" TEXT,'
+        query = query[:-1] + ')'
+        self.c.execute(query)
+        self.conn.commit()
+
+    def fill_air_table(self, fields, table_name, recs, ints, floats, ignores):
+        for record in recs:
+            query = 'INSERT INTO "' + table_name + '"('
+            for def_i in range(0, len(fields)):
+                field = fields[def_i]
+                if field in ignores:
+                    if field == 'Attachments':
+                        query = query + ' "' + field + '",'
+                    else:
+                        pass
+                else:
+                    query = query + ' "' + field + '",'
+            query = query[:-1] + ') VALUES ('
+            for def_i in range(0, len(fields)):
+                field = fields[def_i]
+                if field in ignores:
+                    if field == 'Attachments':
+                        query = query + '?,'
+                    else:
+                        pass
+                else:
+                    query = query + '?,'
+            query = query[:-1] + ')'
+            self.c.execute(query, record)
+        self.conn.commit()
